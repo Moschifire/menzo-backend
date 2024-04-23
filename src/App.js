94433131193c63917cd0343c1d37171925045c61
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2024-04-23 12:53:34 +0100

    ObjectForm and ObjectList implemented on App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ObjectForm from './ObjectForm';
+import ObjectList from './ObjectList';
 
 function App() {
+  const objects = ['Object 1', 'Object 2', 'Object 3'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ObjectForm />
+      <ObjectList objects={objects} />
     </div>
   );
 }
