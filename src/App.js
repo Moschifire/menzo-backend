0ea7b05ce9f0c05d3fc9e03d2899997376eb1504
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2024-04-23 13:44:59 +0100

    ObjectForm updated to update objects, ObjectList updated to use key on App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import ObjectForm from './ObjectForm';
-import ObjectList from './ObjectList';
+import React, { useState } from "react";
+import "./App.css";
+import ObjectForm from "./ObjectForm";
+import ObjectList from "./ObjectList";
 
 function App() {
-  const objects = ['Object 1', 'Object 2', 'Object 3'];
+  const [key, setKey] = useState(0); // State to force re-render of ObjectList
+  const updateObjects = () => setKey((prevKey) => prevKey + 1); // Function to update ObjectList
+
   return (
     <div className="App">
-      <ObjectForm />
-      <ObjectList objects={objects} />
+      <ObjectForm updateObjects={updateObjects} />
+      <ObjectList key={key} />
     </div>
   );
 }
